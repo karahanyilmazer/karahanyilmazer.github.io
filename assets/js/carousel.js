03ca547dcb74d84783ade5f2eb1e90d953ffede1
Karahan Yilmazer <karahan.yilmazer@tum.de>
2023-11-17 12:09:15 +0100

    fix: Image carousel on the main page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,37 +1,8 @@
 $(document).ready(function(){
-  $('.carousel-center').slick({
-      asNavFor: '.carousel-nav',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-      });
-      $('.carousel-nav').slick({
-        asNavFor: '.carousel-center',
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+  $('.carousel-auto').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
